b4c17e4d34d661534d29fd3f74b97236abf99417
trungle1612 <lexuantrung1612@gmail.com>
2021-07-29 01:09:40 +0700

    Update config

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,7 +5,7 @@ class Portfolio < ApplicationRecord
   validates_presence_of :introduce
   validates :p_type, :inclusion => { in: TYPES }
 
-  has_attached_file :photo, styles: { quality: :normal }, storage: :imgur
+  has_attached_file :photo, storage: :imgur
   validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
   has_attached_file :pdf
