35f56faadac827462f973201d580a0c717cd9757
trungle1612 <lexuantrung1612@gmail.com>
2022-02-14 18:27:04 +0700

    Update server's IP

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '139.99.72.177', user: 'ubuntu', roles: [:web, :app, :db], primary: true
+server '139.99.61.48', user: 'ubuntu', roles: [:web, :app, :db], primary: true
 
 
 
